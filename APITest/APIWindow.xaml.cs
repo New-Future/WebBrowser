a4789b76af9bc9ebbada0d274a81769bff50268b
New Future <new_future@msn.cn>
2015-12-15 22:26:49 +0800

    gif图支持

--- APITest/APIWindow.xaml.cs
+++ APITest/APIWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Threading;
 using Http;
-
+using WpfAnimatedGif;
+using System.Linq;
 namespace APITest
 {
     /// <summary>
@@ -12,6 +16,65 @@ namespace APITest
     /// </summary>
     public partial class APIWindow : Window
     {
+        /// <summary>
+        /// 响应数据
+        /// </summary>
+
+        private void SetResponse(Response value)
+        {
+            if (value == null) return;
+            HeadText.Text = value.Headers;
+            string type = value.Header.ContainsKey("CONTENT-TYPE") ? value.Header["CONTENT-TYPE"].TrimStart() : "";
+            image.Visibility = Visibility.Hidden;
+
+            if (type.StartsWith("image/"))
+            {
+                BodyText.Text = "[图片:" + type.Substring(6) + "]";
+
+                var thread = new Thread(() =>
+                   {
+                       this.Dispatcher.Invoke(new Action(() =>
+                       {
+                           SetTitle("解析图片" + URL);
+                           BitmapImage bimg = new BitmapImage();
+                           bimg.BeginInit();
+                           bimg.StreamSource = new MemoryStream(value.Data);
+                           bimg.EndInit();
+                           bimg.Freeze();
+                           //image.Source = bimg;
+                           ImageBehavior.SetAnimatedSource(image, bimg);
+                           //image.Sour
+                           image.Visibility = Visibility.Visible;
+                           SetTitle("[图片]" + URL);
+                       }));
+                   });
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            else
+            {
+                BodyText.Text = value.Body;
+                image.Source = null;
+            }
+        }
+
+
+        /// <summary>
+        /// 获取或者修改URL
+        /// </summary>
+        public string URL
+        {
+            get
+            {
+                var url = URLBox.Text.Trim();
+                if (!url.StartsWith("http"))
+                {
+                    url = "https://" + url;
+                }
+                return url;
+            }
+            set { URLBox.Text = value.Trim(); }
+        }
         private string method = "GET";
         public APIWindow()
         {
@@ -84,10 +147,9 @@ namespace APITest
         /// <param name="e"></param>
         private void SendBtn_Click(object sender, RoutedEventArgs e)
         {
-            string url = URLBox.Text.TrimStart();
-            Uri uri = new Uri(url);
+            string url = URL;
             HTTP http = new HTTP();
-      
+            SetTitle("正在请求" + url);
             /*添加自定义头*/
             var addHeaders = GetAddHeaders();
             foreach (var item in addHeaders)
@@ -97,25 +159,27 @@ namespace APITest
             /*自定义字段*/
             var Data = GetFields();
             Response response;
-            if (radioButtonGet.IsChecked==true)
+            if (radioButtonGet.IsChecked == true)
             {
                 //GET请求
                 response = http.GET(url, Data);
-
-            }else if(true==radioButtonHead.IsChecked)
+            }
+            else if (true == radioButtonHead.IsChecked)
             {
                 //HEAD请求
                 response = http.HEAD(url);
-            }else if(true== radioButtonPost.IsChecked)
+            }
+            else if (true == radioButtonPost.IsChecked)
             {
                 //POST请求
                 response = http.POST(url, Data);
-            }else if(true == radioButtonPut.IsChecked)
+            }
+            else if (true == radioButtonPut.IsChecked)
             {
                 //PUT请求
                 response = http.PUT(url, Data);
             }
-            else if(true == radioButtonDelete.IsChecked)
+            else if (true == radioButtonDelete.IsChecked)
             {
                 response = http.DELETE(url);
             }
@@ -125,17 +189,20 @@ namespace APITest
                 response = http.Request(url);
             }
 
+            SetTitle("请求结束" + url);
             SendText.Text = http.SendData;
+            SendText.UpdateLayout();
             if (response == null)
             {
                 MessageBox.Show(http.LastError, "请求异常");
             }
             else
             {
-                HeadText.Text = response.Headers;
-                BodyText.Text = response.Body;
+                SetResponse(response);
             }
+            SetTitle(url);
         }
+
         /// <summary>
         /// 绑定回车事件
         /// </summary>
@@ -158,7 +225,7 @@ namespace APITest
         {
             method = (sender as RadioButton).Content.ToString();
             //POST和GET,PUT可以设置字段，其他不可以
-            if(method=="GET"||method=="POST"||method=="PUT")
+            if (method == "GET" || method == "POST" || method == "PUT")
             {
                 addField.IsEnabled = true;
             }
@@ -168,6 +235,23 @@ namespace APITest
             }
         }
 
-       
+        /// <summary>
+        /// 设置标题
+        /// </summary>
+        /// <param name="title"></param>
+        private void SetTitle(string title)
+        {
+            this.Title = title;
+        }
+
+        /// <summary>
+        /// 关闭图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            image.Visibility = Visibility.Hidden;
+        }
     }
 }
